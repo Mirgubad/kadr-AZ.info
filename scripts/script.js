61b43968afa5661e6d37db4f7bb9e2c4198b864c
Mirgubad <mirgubadma@code.edu.az>
2023-03-24 06:41:47 +0400

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,15 @@ $(".slick-wrap").on("init", function (event, slick) {
       .addClass("heading" + k);
   });
 });
+
+
+
 $(".slick-wrap").slick({
   focusOnSelect: true,
   infinite: true,
   arrows: true,
+  prevArrow:"<button class='slick-prev slick-arrow'  type='button' ><i class='fas fa-angle-left'></i></button>",
+  nextArrow:"<button class='slick-next slick-arrow'  type='button' ><i class='fas fa-angle-right'></i></button>",
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
@@ -18,40 +23,47 @@ $(".slick-wrap").slick({
   centerPadding: "30px",
 });
 
-var slickNext = document.querySelector(".slick-next");
-var slickPrev = document.querySelector(".slick-prev");
+$(".slick-wrap-mobile").slick({
+  focusOnSelect: true,
+  infinite: true,
+  arrows: true,
+  prevArrow:"<button class='slick-prev slick-arrow'  type='button' ><i class='fas fa-angle-left'></i></button>",
+  nextArrow:"<button class='slick-next slick-arrow'  type='button' ><i class='fas fa-angle-right'></i></button>",
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "30px",
+});
 
-slickPrev.innerHTML = `<i class="fas fa-angle-left"></i>`;
-slickNext.innerHTML = `<i class="fas fa-angle-right"></i>`;
 
 var searchBtn = document.getElementById("search_btn");
 var searchPanel = document.querySelector(".search");
-var body=document.getElementsByTagName("body")
+var body = document.getElementsByTagName("body");
 
 $("#search_btn").click(function () {
   if (searchPanel.style.display != "block") {
     $(".search").slideDown(500);
-    console.log("Sa");
-    $(this).html(`<i class="fa-solid fa-xmark"></i> Bağla`);
+    $(this).html(`<i class="fa-solid fa-xmark"></i> <span>Bağla</span>`);
   } else {
     $(".search").slideUp(500);
-    $(this).html(`<i class="fas fa-search"></i> Saytda Axtarış`);
+    $(this).html(`<i class="fas fa-search"></i> <span>Saytda Axtarış</span>`);
   }
 });
 
-$(".change_theme").click(function(){
-$("body").toggleClass("dark_body")
-$(".logo").toggleClass("text-light")
-$("h4").toggleClass("text-light")
-$(".vacancy-item").toggleClass("text-light")
-$(".slick-inner").toggleClass("text-light")
-$(".add-fav-btn").toggleClass("text-dark")
-$("#search_btn").toggleClass("bg-light text-dark")
-$(this).toggleClass("text-dark bg-light")
+$(".change_theme").click(function () {
+  $("body").toggleClass("dark_body");
+  $(".logo").toggleClass("text-light");
+  $("h4").toggleClass("text-light");
+  $(".vacancy-item").toggleClass("text-light");
+  $(".slick-inner").toggleClass("text-light");
+  $(".add-fav-btn").toggleClass("text-dark");
+  $("#search_btn").toggleClass("bg-light text-dark");
+  $(this).toggleClass("text-dark bg-light");
 
-if($("body").hasClass("dark_body")){
-  $(this).html(`<i class="fa-solid fa-sun"></i>`)
-}else{
-  $(this).html(`<i class="fas fa-moon"></i>`)
-}
-})
+  if ($("body").hasClass("dark_body")) {
+    $(this).html(`<i class="fa-solid fa-sun"></i>`);
+  } else {
+    $(this).html(`<i class="fas fa-moon"></i>`);
+  }
+});
